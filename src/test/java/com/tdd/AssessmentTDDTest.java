348c6e7fad10cff9ce566a39d6e96b5014411641
shamsul.haque <shamsul1993@gmail.com>
2020-05-12 06:48:26 +0530

    commit 5

--- src/test/java/com/tdd/AssessmentTDDTest.java
+++ src/test/java/com/tdd/AssessmentTDDTest.java
@@ -11,6 +11,9 @@ public class AssessmentTDDTest {
         assertTrue(assessmentTDD.checkIfLeapYear(2000));
         assertFalse(assessmentTDD.checkIfLeapYear(2001));
         assertTrue(assessmentTDD.checkIfLeapYear(2004));
+        assertFalse(assessmentTDD.checkIfLeapYear(1700));
+        assertFalse(assessmentTDD.checkIfLeapYear(1800));
+        assertFalse(assessmentTDD.checkIfLeapYear(1900));
 
     }
 }

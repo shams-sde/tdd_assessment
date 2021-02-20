0d948062b2267d49f14b9cd19cfb2dee8df783ca
shamsul.haque <shamsul1993@gmail.com>
2021-02-20 10:35:23 +0530

    commit 2

--- src/main/java/com/tdd/StringCalculatorTDD.java
+++ src/main/java/com/tdd/StringCalculatorTDD.java
@@ -1,4 +1,11 @@
 package com.tdd;
 
 public class StringCalculatorTDD {
+
+    int add(String numbers) {
+        int sum = 0;
+        if(numbers.isEmpty()) return sum;
+
+        return sum;
+    }
 }
--- src/test/java/com/tdd/StringCalculatorTDDTest.java
+++ src/test/java/com/tdd/StringCalculatorTDDTest.java
@@ -1,13 +1,13 @@
 package com.tdd;
 
 import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class StringCalculatorTDDTest {
 
     @Test
     public void shouldAdd() {
         StringCalculatorTDD stringCalculatorTDD = new StringCalculatorTDD();
+        assertEquals(stringCalculatorTDD.add(""), 0);
     }
 }

986b2fe2299ba065e6610a39e1438797645c5d8b
shamsul.haque <shamsul1993@gmail.com>
2021-02-20 10:24:38 +0530

    commit 1

---
+++ src/test/java/com/tdd/StringCalculatorTDDTest.java
@@ -0,0 +1,13 @@
+package com.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class StringCalculatorTDDTest {
+
+    @Test
+    public void shouldAdd() {
+        StringCalculatorTDD stringCalculatorTDD = new StringCalculatorTDD();
+    }
+}

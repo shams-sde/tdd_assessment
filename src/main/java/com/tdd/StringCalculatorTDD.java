97b9cffae9be5fd731e2649d5a5587bafc1e3463
shamsul.haque <shamsul1993@gmail.com>
2021-02-20 11:43:35 +0530

    commit 6

--- src/main/java/com/tdd/StringCalculatorTDD.java
+++ src/main/java/com/tdd/StringCalculatorTDD.java
@@ -4,22 +4,30 @@ import java.util.Scanner;
 
 public class StringCalculatorTDD {
 
-    int add(String numbers) {
+    int add(String numbers) throws Exception{
         int sum = 0;
         if(numbers.isEmpty()) return sum;
 
         try (Scanner scanner = new Scanner(numbers)) {
             String delimiter = ",";
+            StringBuilder negatives = new StringBuilder();
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 if (line.contains("//")) {
                     delimiter = line.replace("//", "").trim();
                     continue;
                 }
-                for (String num : line.split(delimiter)) {
-                    sum += Integer.parseInt(num.trim());
+                for (String data : line.split(delimiter)) {
+                    int num = Integer.parseInt(data.trim());
+                    if (num < 0) {
+                        negatives.append(data + ", ");
+                        continue;
+                    } else if (negatives.length() == 0) {
+                        sum += num;
+                    }
                 }
             }
+            if (negatives.length() > 0) throw new Exception("negatives not allowed : "+negatives);
         }
 
         return sum;
--- src/test/java/com/tdd/StringCalculatorTDDTest.java
+++ src/test/java/com/tdd/StringCalculatorTDDTest.java
@@ -1,12 +1,18 @@
 package com.tdd;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.rules.ExpectedException;
+
 import static org.junit.Assert.*;
 
 public class StringCalculatorTDDTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void shouldAdd() {
+    public void shouldAdd() throws Exception{
         StringCalculatorTDD stringCalculatorTDD = new StringCalculatorTDD();
         assertEquals(stringCalculatorTDD.add(""), 0);
         assertEquals(stringCalculatorTDD.add("1"), 1);
@@ -14,5 +20,8 @@ public class StringCalculatorTDDTest {
         assertEquals(stringCalculatorTDD.add("1\n2,3"), 6);
         assertEquals(stringCalculatorTDD.add("1\n,"), 1);
         assertEquals(stringCalculatorTDD.add("//;\n1;2"), 3);
+        Assertions.assertThrows(Exception.class, () -> {
+            stringCalculatorTDD.add("-1, -5");
+        });
     }
 }

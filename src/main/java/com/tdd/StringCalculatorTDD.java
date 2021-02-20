132f25b0a684b59358e07a855b70c42a33b855f2
shamsul.haque <shamsul1993@gmail.com>
2021-02-20 10:56:43 +0530

    commit 4

--- src/main/java/com/tdd/StringCalculatorTDD.java
+++ src/main/java/com/tdd/StringCalculatorTDD.java
@@ -1,13 +1,20 @@
 package com.tdd;
 
+import java.util.Scanner;
+
 public class StringCalculatorTDD {
 
     int add(String numbers) {
         int sum = 0;
         if(numbers.isEmpty()) return sum;
 
-        for (String num : numbers.split(",")) {
-            sum += Integer.parseInt(num.trim());
+        try (Scanner scanner = new Scanner(numbers)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                for (String num : line.split(",")) {
+                    sum += Integer.parseInt(num.trim());
+                }
+            }
         }
 
         return sum;
--- src/test/java/com/tdd/StringCalculatorTDDTest.java
+++ src/test/java/com/tdd/StringCalculatorTDDTest.java
@@ -11,5 +11,6 @@ public class StringCalculatorTDDTest {
         assertEquals(stringCalculatorTDD.add(""), 0);
         assertEquals(stringCalculatorTDD.add("1"), 1);
         assertEquals(stringCalculatorTDD.add("1, 5"), 6);
+        assertEquals(stringCalculatorTDD.add("1\n2,3"), 6);
     }
 }
